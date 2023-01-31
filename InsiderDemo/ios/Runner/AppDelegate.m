fb153dc57bb812aa77eab2692d847b0e07ea2619
Melih Karadas <melihkaradas@useinsider.com>
2023-01-31 13:40:30 +0300

    Helper - 1675161205
    Fixed push not appearing issue in foreground state

--- InsiderDemo/ios/Runner/AppDelegate.m
+++ InsiderDemo/ios/Runner/AppDelegate.m
@@ -7,6 +7,8 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+    
+    UNUserNotificationCenter.currentNotificationCenter.delegate = self;
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
